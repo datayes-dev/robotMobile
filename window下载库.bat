e2607a57d17cb6f65b835bfc2d62e5156aff695e
shenen.gao <shenen.gao@datayes.com>
2016-12-16 14:53:37 +0800

    no message

---
+++ window下载库.bat
@@ -0,0 +1,24 @@
+npm install babel-cli
+@pause
+npm install babel babel-preset-es2015
+@pause
+npm install babel webpack
+@pause
+npm install babel @angular/common
+@pause
+npm install babel @angular/compiler
+@pause
+npm install babel @angular/core
+@pause
+npm install babel @angular/forms
+@pause
+npm install babel @angular/http
+@pause
+npm install babel @angular/platform-browser
+@pause
+npm install babel @angular/platform-browser-dynamic
+@pause
+npm install babel @angular/router
+@pause
+npm install babel @angular/upgrade
+@pause
