ab6289836b55d4abcceadcfc8dfd71583ce9753c
shenen.gao <shenen.gao@datayes.com>
2016-12-27 09:40:09 +0800

    提交 配置文件

---
+++ src/config.js
@@ -0,0 +1,33 @@
+
+//PRD, STG, QA
+let const CUR_BASE = "STG";
+
+//网络请求BaseUrl
+export const BASE_URL = {
+
+	PRD: "https://gw.wmcloud.com",
+	STG: "https://gw.wmcloud-stg.com",
+	QA: "https://gw.wmcloud-stg.com"
+};
+
+//子服务器 url
+export const SUB_SERVICE_URL = {
+
+	NOMRAL: "",
+    RRP: "/rrp/mobile",
+    MOBILE: {
+    	PRD: "/ira/mobile",
+		STG: "/rrpqa/mobile",
+		QA: "/ira/mobile"
+    },
+    MOBILE_MASTER: "/mobilemaster",
+    CLOUD: "/cloud",
+    USER_MASTER: "/usermaster",
+    WEB_VIEW_PAGE_INFO: "/information/news/",
+    WEB_VIEW_PAGE_RESEARCH: "/information/research/",
+    WEB_VIEW_PAGE_ANNOUNCEMENT: "/information/announcement/",
+    WEB_VIEW_PAGE_EVENT: "",
+    WEB_MIAL: "/webmail"
+};
+
+
