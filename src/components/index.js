5e71a9cacc74415a58317516c5e73d052268c922
zhizhong.zhou <ios.dev@datayes.com>
2016-12-27 19:51:17 +0800

    类名更换

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,5 @@ export Loading from './Loading/Loading';
 export SimpleTitleBar from './title/SimpleTitleBar';
 export InputTitleBar from './title/InputTitleBar';
 export SimpleTabBar from './tab/SimpleTabBar';
-export SearchBaseInfoListView from './SearchBaseInfoListView/SearchBaseInfoListView';
+export SingleStringClickCell from './cell/content/SingleStringClickCell';
 export CellBlueFoldTitle from './cell/title/CellBlueFoldTitle';
