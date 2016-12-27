793dd2493c037aad19439fdc337585dfa7f3ff34
shenen.gao <shenen.gao@datayes.com>
2016-12-27 20:50:44 +0800

    提交相关公司

--- src/components/cell/content/ThreeStringClickCell0.js
+++ src/components/cell/content/ThreeStringClickCell0.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ThreeStringClickCell0.less';
 
 //连续3个字符串的小cell 控件，支持点击事件 （相关公司用）
 export default class ThreeStringClickCell0 extends React.Component {
@@ -8,18 +9,43 @@ export default class ThreeStringClickCell0 extends React.Component {
 	}
 
 	render() {
+
+		let contentJXS = new Array();
+		
+		if (this.props.contents != null) {
+
+			let contents = this.props.contents;
+			let len = contents.length;
+
+			for (var i = 0; i < len; i++) {
+				
+				let content = contents[i];
+
+				if (content instanceof Array && content.length >= 3) {
+
+					contentJXS.push(
+						<tr key={i}>
+	                        <td>{content[0]}</td>
+	                        <td>{content[1]}</td>
+	                        <td>{content[2]}
+	                        	<nt></nt>
+	                        </td>
+	                    </tr>
+					);
+				}
+			}
+		}
+
 		return (
 			<div className="XGYWGS TableBackground">
-				<table>
-		  			<tbody>
-		  				{this.props.children}
-	                    <tr>
-	                        <td>当代东方(000786)</td>
-	                        <td>201</td>
-	                        <td>50.32<nt></nt></td>
-	                    </tr>
-		  			</tbody>
-	            </table>
+				<div className="TableBackground">
+					<table>
+			  			<tbody>
+			  				{this.props.children}
+		                    { contentJXS }
+			  			</tbody>
+		            </table>
+	            </div>
             </div>
 		);
 	}
--- src/containers/Search/zongHe/cards/CompanyRelativeStock.js
+++ src/containers/Search/zongHe/cards/CompanyRelativeStock.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CellBlueFoldTitle } from 'components';
+import { CellBlueFoldTitle, ThreeStringClickCell0 } from 'components';
 import { request } from 'request';
 
 export default class CompanyRelativeStock extends React.Component {
@@ -7,39 +7,100 @@ export default class CompanyRelativeStock extends React.Component {
 	constructor(props) {
 		super(props);
 
+		this.state = {
+
+			show : false,
+			companyInfos: null
+		}
+
 		this.onMoreClicked = this.onMoreClicked.bind(this);
 	}
 
 	componentDidMount() {
 
-		console.log(this.props);
 		// SEARCH_RELATED_OPERATION
-		// if () {
+		if (this.props.basicInfo != null) {
 
-		// }
+			let _this = this;
+
+			request.start(
+				request.SEARCH_RELATED_OPERATION, 
+				{
+					data: {
+                        pageNow: 1,
+                        pageSize: 5,
+                        partyID: this.props.basicInfo.partyID
+                    },
+                    success: function(response) {
+
+                        if(response.code == 1) {
+
+                        	_this.newWorkFinish(response);
+
+                        } else {
+                            
+                        }
+                    }, 
+                    error: function() {
+
+                    }
+				}
+			);
+		}
+	}
+
+	newWorkFinish(response) {
+
+		if (response != null && response.kMapCompanyOperationRelatedCompaniesInfo != null 
+			&& response.kMapCompanyOperationRelatedCompaniesInfo.operationRelatedCompaniesList != null) {
+
+			this.setState({
+				show : true,
+				companyInfos: response.kMapCompanyOperationRelatedCompaniesInfo.operationRelatedCompaniesList
+			});
+		}
 	}
 
 	onMoreClicked() {
 
+		//todo 跳转到相关公司的落地页
 	}
 
 	render() {
+
+		let companyInfos = this.state.companyInfos;
+
+		if (!this.state.show || companyInfos == null || (companyInfos != null && companyInfos.length <= 0)) {
+
+			return (
+		      <div></div>
+		    );
+		} 
+
+		let len = companyInfos.length;
+		let contents = new Array();
+
+		for (let i = 0; i < len; ++i) {
+
+			let companyInfo = companyInfos[i];
+			let content = new Array();
+			content.push(companyInfo.shortName + "(" + companyInfo.ticker + ")");
+			content.push(companyInfo.marketValue);
+			content.push(companyInfo.pe);
+
+			contents.push(content);
+		}
+
+
 		return (
-		  	<CellBlueFoldTitle title="相关业务公司" moreCallBack={this.onMoreClicked}>
-		  		<table>
-		  			<tbody>
-		  				<tr>
-	                        <th>作品简称及代码</th>
-	                        <th>总市值(亿元)</th>
-	                        <th>PE(TTM)</th>
-	                    </tr>
-	                    <tr>
-	                        <td>当代东方(000786)</td>
-	                        <td>201</td>
-	                        <td>50.32<nt></nt></td>
-	                    </tr>
-		  			</tbody>
-                </table>
+			<CellBlueFoldTitle title="相关业务公司" moreCallBack={ this.onMoreClicked }>
+		  		<ThreeStringClickCell0 contents={ contents }>
+		  			<tr>
+                        <th>作品简称及代码</th>
+                        <th>总市值(亿元)</th>
+                        <th>PE(TTM)</th>
+                    </tr>
+		  		</ThreeStringClickCell0>
 		  	</CellBlueFoldTitle>
 		);
 	}
