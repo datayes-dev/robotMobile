db428b891e866df59d9695d5a1f0998280fd2d01
zhizhong.zhou <ios.dev@datayes.com>
2016-12-27 19:04:43 +0800

    基本信息组件

---
+++ src/components/SearchBaseInfoListView/SearchBaseInfoListView.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './SearchBaseInfoListView.less';
+
+/**
+
+**/
+export default class SearchBaseInfoListView extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.onTabClicked = this.onTabClicked.bind(this);
+  }
+
+  onTabClicked(event) {
+	
+  	if (event != null && event.target != null && this.props.onTabClicked != null) {
+	 	alert(1);
+  	  		let value = event.target.attributes.value.value;
+  	  		
+  			this.props.onTabClicked(Number(value), this.props.contents[value]);
+  	}
+  }
+  
+  render() {
+  	
+  	let tabsJXS = [];
+  	let contents = this.props.contents;
+  	let hasMores = this.props.hasMores;
+
+  	if (contents != null && contents.length > 0) {
+
+  		for (var i = 0; i < contents.length; i++) {
+  			var moreFlag = false;
+  			if(hasMores != null  && hasMores.length > i){
+  				moreFlag = hasMores[i];
+  			}
+  			tabsJXS[i] = <li key={i} value={i} onClick={this.onTabClicked}><h3> {contents[i]} </h3>
+  			{
+  				moreFlag&&<nt></nt>
+  			}
+  			</li>;
+  		}
+  	}
+  	
+  	
+    return (
+    		<div className="SearchInfoList">
+	    		    <ul>
+                {tabsJXS}
+                </ul>
+	     </div>
+    );
+  }
+}
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export SimpleTitleBar from './title/SimpleTitleBar';
 export InputTitleBar from './title/InputTitleBar';
 export SimpleTabBar from './tab/SimpleTabBar';
 export Search_Cell_HeaderView from './Search_Cell_HeaderView/Search_Cell_HeaderView';
+export SearchBaseInfoListView from './SearchBaseInfoListView/SearchBaseInfoListView';
