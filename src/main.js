cb56a55490d2b0fe7212611b22cdb973f6767928
shenen.gao <shenen.gao@datayes.com>
2016-12-27 09:48:12 +0800

    jquery提交

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
+
+//CSS
+import 'normalize.css';
+import 'css/mui.css';
+
+//jquery
+import $ from 'jquery';
+window._$ = $;
+
+//React
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, IndexRedirect, hashHistory } from 'react-router';
-import 'normalize.css';
-import 'css/mui.css';
 import { App, Home, Search } from './containers';
-//import { Search, SearchAnnouncement, SearchData, SearchNews, SearchZongHe } from './containers';
 
-//<IndexRedirect to="/search/zonghe" />
-//<Route path="/search/zonghe" component={SearchZongHe}/>
-//<Route path="/search/data" component={SearchData}/>
-//<Route path="/search/announcement" component={SearchAnnouncement}/>
-//<Route path="/search/news" component={SearchNews}/>
 
 ReactDOM.render((
     <Router history={hashHistory}>
@@ -21,5 +23,14 @@ ReactDOM.render((
         </Route>
     </Router>
 ), document.getElementById('root'));
+
+
+////import { Search, SearchAnnouncement, SearchData, SearchNews, SearchZongHe } from './containers';
+
+//<IndexRedirect to="/search/zonghe" />
+//<Route path="/search/zonghe" component={SearchZongHe}/>
+//<Route path="/search/data" component={SearchData}/>
+//<Route path="/search/announcement" component={SearchAnnouncement}/>
+//<Route path="/search/news" component={SearchNews}/>
 
 
